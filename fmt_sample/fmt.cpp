d2fdc8eb46023be5fc6f9f9c00427a479cc79fc4
LiangTsao <LiangTsao@qq.com>
2025-05-08 15:15:03 +0800

    ss

--- fmt_sample/fmt.cpp
+++ fmt_sample/fmt.cpp
@@ -5,9 +5,9 @@ int main() {
     fmt::print("Hello, {}!\n", "world");
 
     // 打印整数和浮点数
-    int number = 42;
-    double pi = 3.14159;
-    fmt::print("Number: {}, Pi: {:.2f}\n", number, pi);
+    int number = 4321;
+    double pi = 4.14159555555;
+    fmt::print("Number: {}, Pi: {:.5f}\n", number, pi);
 
     return 0;
 }
